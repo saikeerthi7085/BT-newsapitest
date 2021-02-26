6ddf5ada2c093bb03d9f4cdae3d1bfc777d28940
maddineni7085 <saikeerthimaddineni@gmail.com>
2021-02-26 14:58:33 +0000

    Updates

---
+++ precache-manifest.92829ce77308e5791ed6b6d6a6fc5c0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258151b1100573865d0c4fef854e3e0d",
+    "url": "/BT-newsapitest/index.html"
+  },
+  {
+    "revision": "dfc38797726d078cb639",
+    "url": "/BT-newsapitest/static/css/main.4fab0b79.chunk.css"
+  },
+  {
+    "revision": "f128941e6ad27d631988",
+    "url": "/BT-newsapitest/static/js/2.a1828aee.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/BT-newsapitest/static/js/2.a1828aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc38797726d078cb639",
+    "url": "/BT-newsapitest/static/js/main.692c9975.chunk.js"
+  },
+  {
+    "revision": "1fb63e9c7fbbbf8eecfe",
+    "url": "/BT-newsapitest/static/js/runtime-main.3928e434.js"
+  }
+]);
